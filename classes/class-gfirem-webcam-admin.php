8cc1d7f94d4b040ef917ff75cbaeafa7a248ede0
Guillermo <gfirem@gmail.com>
2018-03-21 15:05:53 -0300

    * Adding the red color to the fidl inside the form builder. * Refactoring the code.

--- classes/class-gfirem-webcam-admin.php
+++ classes/class-gfirem-webcam-admin.php
@@ -19,14 +19,14 @@ class GFireMWebcamAdmin {
 
     function  __construct(){
         add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_js' ) );
-       // add_action( 'admin_enqueue_scripts', array( $this, 'front_enqueue_style' ) );
-
     }
 
     public function enqueue_js( $hook ) {
-
-        wp_enqueue_script( 'gfirem_webcam_admin', GFireM_Webcam::$assets . 'js/camera_admin.js', array( "jquery" ), GFireM_Webcam::getVersion(), true );
-
-
+	    $current_screen = get_current_screen();
+	    if ( 'toplevel_page_formidable' === $current_screen->id ) {
+		    wp_enqueue_style( 'gfirem_webcam_admin_css', GFireM_Webcam::$assets . 'css/gfirem-webcam.css' );
+		    wp_enqueue_script( 'gfirem_webcam_admin', GFireM_Webcam::$assets . 'js/camera_admin.js', array( "jquery" ), GFireM_Webcam::getVersion(), true );
+	    }
+     
     }
 }
